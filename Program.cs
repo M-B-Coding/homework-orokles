03cbaebef8556248f9a69434ac1ce2e38327e151
Miklós Kiss <48698373+mijki@users.noreply.github.com>
2023-03-10 15:42:40 +0100

    Add original files
    These were the original files of the homework

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orokles
+{
+    class Program
+    {
+        static void Teszt1()
+        {
+            Terkep terkep = new Terkep(80, 25);
+            TerkepRajzolo rajzolo = new TerkepRajzolo(terkep);
+            rajzolo.Kirajzol();
+        }
+
+        static void Main(string[] args)
+        {
+            Teszt1();
+        }
+    }
+}
